d69bf674e528276169943f21cd851c5b9c3aa2f2
Saintpiogabo93 <tocata_fuga_dorian@hotmail.com>
2024-12-22 00:57:44 -0600

    Se corrigio el codigo y el archivo delay

--- Rotacion_Bits.asm
+++ Rotacion_Bits.asm
@@ -1,42 +1,36 @@
-; CONFIG
-; __config 0xFF59
- __CONFIG _FOSC_INTOSCCLK & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _BOREN_ON & _LVP_OFF & _CPD_OFF & _CP_OFF
+; CONFIGURACIÓN
+ __CONFIG _INTRC_OSC_NOCLKOUT & _WDTE_OFF & _PWRTE_ON & _MCLRE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _CP_OFF
 
-;Configuracion de variables para el codigo
-    cblock  0x20    ;
-    conta
-    endc
+ LIST P=16F628A
+ #include <p16f628a.inc>
 
-;Configuracion del pic y todas sus palabras claves
-    list p=16f628a
-    #include "/opt/microchip/mplabx/v5.20/mpasmx/p16f628a.inc"
-    
-;Iniciamos el programa
-    bsf		STATUS, 5 ; Vamos al banco 1
-    clrf	TRISB	  ; Configuramos como salida todo el PORTB
-    ; Como vamos a utilizar el TMR0, cargamos las instrucciones pertinentes
-    movlw	b'00000111'
-    movwf	OPTION_REG
-    bcf		STATUS, 5 ; Regresamos al banco 0
-    
-;Creamos una funcion o subrutina para rotar los bits
+ ; VARIABLES
+ cblock 0x20
+    CONTA           ; Contador para LEDs
+ endc
+
+ ; INICIO DEL PROGRAMA
+ org 0x00
+    bsf		STATUS, 5       ; Cambiar al banco 1
+    clrf	TRISB           ; Configurar PORTB como salida
+    bcf		STATUS, 5       ; Regresar al banco 0
+
 inicio:
-    movlw	.8	  ; Cargamos en registro el numero de bits o LEDs que vamos a utilizar
-    movwf	conta	  ; Escribimos el contenido en la variable que creamos
-    movlw	b'00000001';Escribimos la configuracion de bits que vamos a dejar en el PORTB
+    movlw	.8              ; Número de LEDs o rotaciones
+    movwf	CONTA           ; Guardar en contador
+    movlw	b'00000001'     ; Configuración inicial de PORTB para que el primer LED empiece encendido
     movwf	PORTB
-    movlw	.500	  ; Escribimos los milisegundos que vamos a utilizar
-    
-;Llamamos a nuestra funcion del archivo separado
-    call	delay_ms
-    decfsz	conta, 1  ; Decrementamos el contenido del registro F en uno y si nos da cero salta una instruccion
-    goto	$+2	  ; Saltamos dos instrucciones mas adelante para llgar a bsf STATUS, 0
-    goto	inicio	  ; Vamos a inicio 
-    bsf		STATUS, 0 ; Con esto nos aseguramos que C = 1, para cuando se roten los bits
-    rlf		PORTB, 1  ; Rotamos los bits
-    goto	$-7	  ; Regresamos 7 instrucciones que serian los milisegundos
-    
-;Incluimos la libreria para el delay
-    #include <delay.inc>
-    
-    end
+bucle_principal:
+    movlw	.150            ; Configurar retardo
+    call	DELAY_MS        ; Llamar a la rutina de retardo
+    decfsz	CONTA, F        ; Decrementar CONTA, saltar si llega a 0
+    goto	$+2             ; Saltar las siguientes dos instrucciones
+    goto	inicio          ; Reiniciar el programa
+    bcf		STATUS, 0       ; C=1 (Carry). Colocamos bsf para que se queden prendidos.
+    rlf		PORTB, F        ; Rotar bits a la izquierda
+    goto	bucle_principal ; Regresar al bucle principal
+
+ ; INCLUIR RUTINAS DE RETARDO
+ #include "delay.inc"
+
+ end
